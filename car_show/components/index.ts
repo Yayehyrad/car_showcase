793288ac5e9c8d09fe80c537e3d72fc738e6fda2
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-07-10 05:46:53 -1200

    update

--- car_show/components/index.ts
+++ car_show/components/index.ts
@@ -2,11 +2,14 @@ import Hero from "./Hero";
 import CustomBtn from "./CustomBtn";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
 
 export {
     Hero,
     CustomBtn,
     NavBar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar
 }
